8cd652ac8b5b2b2a6febfee501ca4f13ab2ce841
EdwinP7 <edwinperea7@gmail.com>
2018-05-28 16:14:55 -0700

    Deployment

--- parserz.py
+++ parserz.py
@@ -6,42 +6,59 @@ from bs4 import BeautifulSoup
 juicy_responses = [
     'Juiced up feck.',
     'Juiced to the gills.',
+    'Juicy little slut',
     '👀👀👀👀👀👀👀👀👀👀',
     '💉💉💉💉💉💉💉💉💉💉',
+    '🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀'
 ]
 
 non_juicy_responses = [
     'It\'s just the angle, bro.',
+    'I didn\'t find anything.',
+    'Either this person hasn\'t been sanctioned (yet) or you misspelled their name.',
 ]
 
 fun_bites = {
-    'brock lesnar': 'He\'s just a jacked up white boy. Deal with it.',
+    'lesnar, brock': 'He\'s just a jacked up white boy. Deal with it.',
     'bad bot': [
         'How bout u go an fuck off my page then u peice of shit u think \
          I need a stupid fuckwitt like u telling me about being bad who the \
          fuck are u take your worthless advice and get the fuck out of here',
         '[Okay](https://j.gifs.com/VPZxyX.gif)',
-        '{user} I\'m Sitting on about $8 million cash, I have $2 million in \
-        real estate. I won the UFC belt nine times. I\'m internationally famous. \
+        'u/{user} I\'m Sitting on about $8 million bitcoin, I have $2 million in \
+        reddit gold. I won the UFC belt nine times. I\'m internationally famous. \
         I\'m 6\'4. You on the other hand: i\'m guessing 5 feet flat, I\'m guessing \
         with the net worth of $100,000. Never won any titles, can walk around \
         without anyone giving a fuck who you are or what you do. my point is, \
-        you and I as UFC athletes are not even close to being on the same level. \
+        you and I as redditors are not even close to being on the same level. \
         I\'m guessing that\'s why you are constantly attacking me, it\'s \
         understandable. You\'re literally and figuratively like a boy \
         compared to someone like me. I\'m actually questioning myself as \
         I\'m writing this thinking "why am I even giving you the time of day" \
         every time I turn around you are writing me some bullshit, why do \
         you try so hard to get my attention?',
+        'that\'s funny coming from a juicy little slut like u would love u \
+        to say anything to my face fucken cheating little betch u another \
+        steroid usin bitch look at your pathetic bitch ass',
+        'Lol you wear turtlenecks',
     ],
+    'diaz, nick': '[A gentleman never tells. ^(A gentleman never tells, nah...)](https://www.youtube.com/watch?v=piEb_XdZsMQ&feature=youtu.be&t=27)',
 
 }
 
 reasons_fun_bites = {
-    'cannabinoids': [
-        'http://i.imgur.com/WY3lX.gif',
-        'https://i.imgur.com/ICbfkUY.gifv',
+    'cannabidiol': [
+        '[🚭🚭🚭...](http://i.imgur.com/WY3lX.gif)',
+        '[🌲🌲🌲...](https://i.imgur.com/ICbfkUY.gifv)',
     ],
+    'carboxy': [
+        '[🚭🚭🚭...](http://i.imgur.com/WY3lX.gif)',
+        '[🌲🌲🌲...](https://i.imgur.com/ICbfkUY.gifv)',
+    ],
+    'thc': [
+        '[🚭🚭🚭...](http://i.imgur.com/WY3lX.gif)',
+        '[🌲🌲🌲...](https://i.imgur.com/ICbfkUY.gifv)',
+    ]
 }
 
 non_fault = [
@@ -53,12 +70,10 @@ def build_juicy_response(sanctions):
     terms = sanctions[1]
     name = sanctions[2]
 
-    """'|Tables|Are|Cool|\n|:-|:-|:-|\n|col|col|col|'"""
-
     if name.lower() in fun_bites:
         header = fun_bites[name.lower()]
     elif 'Non-Analytical' in reasons_list:
-        header = '[Look into it.](https://imgur.com/gallery/BDjhxfJ)'
+        header = '[Ehhhh..](https://imgur.com/gallery/BDjhxfJ)'
     elif terms in non_fault:
         header_bite = random.randint(0, len(non_juicy_responses) - 1)
         header = non_juicy_responses[header_bite]
@@ -67,14 +82,13 @@ def build_juicy_response(sanctions):
         header = juicy_responses[header_bite]
 
     reasons = re.sub(r'[^\w]', ' ', reasons_list).lower().split(' ')
-
     for key in reasons_fun_bites:
         if key in reasons:
             bite = random.randint(0, len(reasons_fun_bites[key]) - 1)
             header = reasons_fun_bites[key][bite]
     
-        
-    markdown = '###{header}\n\n#### {name}\nReason(s): {reason}\n\nTerms: {terms}\n'.format(
+    name = re.sub(r'[\n]', '', name)
+    markdown = '{header}\n\n##{name}\nReason(s): {reason}\n\nTerms: {terms}\n'.format(
         header=header,
         name=name, 
         reason=reasons_list,
@@ -83,13 +97,11 @@ def build_juicy_response(sanctions):
     return markdown
 
 
-
-
 def get_bot_response(body):
     
     body_content = body.split(' ')
-    if len(body_content) >= 3:
-        sanctions = get_sanctions(body_content[1], body_content[2])
+    if len(body_content) >= 2:
+        sanctions = get_sanctions(body_content[1].lower(), body_content[2].lower())
         if sanctions is None:
             random_non_juicy = random.randint(0, len(non_juicy_responses) - 1)
             message = non_juicy_responses[random_non_juicy]
@@ -107,28 +119,44 @@ def get_sanctions(firstname, lastname):
     """
     name = '{0} {1}'.format(firstname, lastname)
     table = get_usada_table()
-    results = table.find_all('td',
-                             string='{0}, {1}'.format(lastname, firstname))
+    results = table.find_all('a',
+                             string=lambda x: x and x.lower() == '{0}, {1}'.format(lastname, firstname))
     if results:
-        row_contents = results[0].parent.contents
+        name = results[0].text
+        row_contents = results[0].parent.parent.contents
         reasons = row_contents[3].text
-        terms = row_contents[4].text
+        terms = row_contents[7].text
         return [reasons, terms, name]
     else:
-        return None
-        
-        
+        links = table.find_all('a')
+        result = check_string(links, firstname)
+        if result is not None:
+            name = result.text
+            row_contents = result.parent.parent.contents
+            reasons = row_contents[3].text
+            terms = row_contents[7].text
+            return [reasons, terms, name]
+        else:
+            return None
+    
+def check_string(links, firstname):
+    for link in links:
+        if link.text and re.search(firstname, link.text.lower()):
+            return link
+    return None
+
+    
 
 def get_usada_table():
     """
-        Returns USADA's Sanctions Table as soup
+    Returns USADA's Sanctions Table as soup
     """
 
-    url = 'https://www.usada.org/testing/results/sanctions/'
+    url = 'https://ufc.usada.org/testing/results/sanctions/'
     with urllib.request.urlopen(url) as sanctions:
         html = sanctions.read()
 
     soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find(id='tablepress-1').find('tbody')
+    table = soup.find(id='tablepress-2').find('tbody')
 
     return table
--- usadabot.py
+++ usadabot.py
@@ -22,25 +22,30 @@ def client_login():
 
 def whos_hot(reddit):
 
-    parser = configparser.ConfigParser()
+    parser = configparser.ConfigParser() 
     parser.read(config.CONFIG_FILE)
-    for comment in reddit.subreddit('test').comments(limit=25):
+    for comment in reddit.subreddit('MMA').comments(limit=1000):
         body = comment.body
         
         if not parser.has_option('REPLIES', str(comment.id)):
             if body.lower() == 'bad bot' and comment.parent().author.name == reddit.user.me():
-                bite = random.randint(0, len(parserz.fun_bites['bad bot']) - 1)
+                bite = random.randint(0, len( parserz.fun_bites['bad bot']) - 1)
                 reply = parserz.fun_bites['bad bot'][bite]
                 comment.reply(reply.format(user=comment.author.name))
                 parser['REPLIES'][str(comment.id)] = 'True'
-
-            elif 'usadabot' in body and body.split()[0] == 'usadabot':
-                reply = parserz.get_bot_response(comment.body)
+            elif 'usadabot' in body and body.split()[0] == 'usadabot' and comment.author.name != reddit.user.me():
+                try:
+                    reply = parserz.get_bot_response(comment.body)
+                except:
+                    reply = 'Ya broke it...'
                 comment.reply(reply)
                 parser['REPLIES'][str(comment.id)] = 'True'
 
     with open(config.CONFIG_FILE, 'w') as configfile:
         parser.write(configfile)
 
-reddit = client_login()
-print(whos_hot(reddit))
+
+if __name__ == '__main__':
+    reddit = client_login()
+    while True:
+        whos_hot(reddit)
